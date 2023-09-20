a070004361595d945c46de06bb4450783b710a8a
aloyzio <59551570+aloyzio@users.noreply.github.com>
2023-09-20 11:40:14 -0600

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -3,6 +3,6 @@ terraform {
     bucket = "my-aws-tf-state-bucket2211"
     key = "main"
     region = "us-east-2"
-    dynamodb_table = "my-dynamo-db-table"
+    dynamodb_table = "my-dynamo-db-table2211"
   }
 }
